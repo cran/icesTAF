e6652a935c40938689c66e54432709931fe56b15
Arni Magnusson <arni.magnusson@ices.dk>
2017-12-05 13:34:17 +0000

    version 1.4-1

--- R/div.R
+++ R/div.R
@@ -3,33 +3,51 @@
 #' Divide column values in a data frame with a common number.
 #'
 #' @param x a data frame.
-#' @param cols column names.
+#' @param cols column names, or column indices.
 #' @param by a number to divide with.
+#' @param grep whether \code{cols} is a regular expression.
+#' @param \dots passed to \code{grep()}.
 #'
 #' @return
 #' A data frame similar to \code{x}, after dividing columns \code{cols} by the
 #' number \code{by}.
 #'
 #' @note
-#' Provides notation that is reliable and convenient for modifying a large
-#' number of columns, not repeating column names twice.
+#' Provides notation that is convenient for modifying many columns at once.
 #'
 #' @seealso
-#' \code{\link{sweep}} and \code{\link{transform}} can also be used to
-#' recalculate column values, using a more general and verbose syntax.
+#' \code{\link{transform}} can also be used to recalculate column values, using
+#' a more general and verbose syntax.
 #'
-#' \code{\link{rnd}} is a similar function to round columns.
+#' \code{\link{grep}} is the underlying function used to match column names if
+#' \code{grep} is \code{TRUE}.
+#'
+#' \code{\link{rnd}} is a similar function that rounds columns.
 #'
 #' \code{\link{icesTAF-package}} gives an overview of the package.
 #'
 #' @examples
-#' summary <- div(summary.taf,
-#'   c("Rec","Rec_lo","Rec_hi","TSB","TSB_lo","TSB_hi","SSB","SSB_lo","SSB_hi"))
+#' # These are equivalent:
+#'
+#' x1 <- div(summary.taf, c("Rec","Rec_lo","Rec_hi",
+#'                          "TSB","TSB_lo","TSB_hi",
+#'                          "SSB","SSB_lo","SSB_hi",
+#'                          "Removals","Removals_lo","Removals_hi"))
+#'
+#' x2 <- div(summary.taf, "Rec|TSB|SSB|Removals", grep=TRUE)
+#'
+#' x3 <- div(summary.taf, "Year|Fbar", grep=TRUE, invert=TRUE)
+#'
+#' # Less reliable in scripts if columns have been added/deleted/reordered:
+#'
+#' x4 <- div(summary.taf, 2:13)
 #'
 #' @export
 
-div <- function(x, cols, by=1000)
+div <- function(x, cols, by=1000, grep=FALSE, ...)
 {
+  if(grep)
+    cols <- grep(cols, names(x), ...)
   x[cols] <- x[cols] / by
   x
 }
--- R/rnd.R
+++ R/rnd.R
@@ -3,21 +3,25 @@
 #' Round column values in a data frame.
 #'
 #' @param x a data frame.
-#' @param cols column names.
+#' @param cols column names, or column indices.
 #' @param digits number of decimal places.
+#' @param grep whether \code{cols} is a regular expression.
+#' @param \dots passed to \code{grep()}.
 #'
 #' @return
 #' A data frame similar to \code{x}, after rounding columns \code{cols} to the
 #' number of \code{digits}.
 #'
 #' @note
-#' Provides notation that is reliable and convenient for modifying a large
-#' number of columns, not repeating column names twice.
+#' Provides notation that is convenient for modifying many columns at once.
 #'
 #' @seealso
 #' \code{\link{round}} is the underlying function used to round numbers.
 #'
-#' \code{\link{div}} is a similar function to divide columns with a common
+#' \code{\link{grep}} is the underlying function used to match column names if
+#' \code{grep} is \code{TRUE}.
+#'
+#' \code{\link{div}} is a similar function that divides columns with a common
 #' number.
 #'
 #' \code{\link{icesTAF-package}} gives an overview of the package.
@@ -26,12 +30,31 @@
 #' function to round values for ICES advice sheets.
 #'
 #' @examples
-#' summary <- rnd(summary.taf, c("Fbar", "Fbar_lo", "Fbar_hi"), 2)
+#' # These are equivalent:
+#'
+#' x1 <- rnd(summary.taf, c("Rec","Rec_lo","Rec_hi",
+#'                          "TSB","TSB_lo","TSB_hi",
+#'                          "SSB","SSB_lo","SSB_hi",
+#'                          "Removals","Removals_lo","Removals_hi"))
+#' x1 <- rnd(x1, c("Fbar","Fbar_lo","Fbar_hi"), 3)
+#'
+#' x2 <- rnd(summary.taf, "Rec|TSB|SSB|Removals", grep=TRUE)
+#' x2 <- rnd(x2, "Fbar", 3, grep=TRUE)
+#'
+#' x3 <- rnd(summary.taf, "Fbar", grep=TRUE, invert=TRUE)
+#' x3 <- rnd(x3, "Fbar", 3, grep=TRUE)
+#'
+#' # Less reliable in scripts if columns have been added/deleted/reordered:
+#'
+#' x4 <- rnd(summary.taf, 2:13)
+#' x4 <- rnd(x4, 14:16, 3)
 #'
 #' @export
 
-rnd <- function(x, cols, digits=0)
+rnd <- function(x, cols, digits=0, grep=FALSE, ...)
 {
+  if(grep)
+    cols <- grep(cols, names(x), ...)
   x[cols] <- round(x[cols], digits)
   x
 }
